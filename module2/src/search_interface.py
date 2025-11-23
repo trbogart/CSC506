b0d2f88c216b509fd2094bf2bb8d9963989b1d00
Richard Bogart <trbogart@gmail.com>
2025-11-22 18:39:00 -0700

    improve interface

--- module2/src/search_interface.py
+++ module2/src/search_interface.py
@@ -1,7 +1,7 @@
 import random
 import sys
 from time import perf_counter
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Tuple
 
 import search
 from search_timer import SearchTimer, LinearSearchTimer, BinarySearchTimer, SearchResults
@@ -30,60 +30,67 @@ class SearchInterface:
 
     def print_menu(self):
         binary_prefix = '' if self.is_sorted else 'DISABLED (UNSORTED) - '
+        print('0) Exit')
+        print('1) Print menu')
+        print('2) Set size')
+        print('3) Toggle sorted')
+        print('4) List elements')
+        print('5) Linear search for value')
+        print(f'6) {binary_prefix}Binary search for value')
+        print('7) Run performance tests for linear search (will not effect current list)')
+        print(f'8) {binary_prefix}Run performance tests for binary search (will not effect current list)')
+
+    def process_input(self):
         print('------------------------------------------------------------')
         print(f'List has {len(self.a):,} {'sorted' if self.is_sorted else 'unsorted'} elements')
-        print('0) Exit')
-        print('1) Set size')
-        print('2) Toggle sorted')
-        print('3) List elements')
-        print('4) Linear search for value')
-        print(f'5) {binary_prefix}Binary search for value')
-        print('6) Run performance tests for linear search (will not effect current list)')
-        print(f'7) {binary_prefix}Run performance tests for binary search (will not effect current list)')
-        cmd = self.get_int_input(0, 7)
+        cmd = self.get_int_input(0, 7, prompt=f'Enter a command between 0 and 8 (0 to exit and 1 to print menu): ')
         if cmd == 0:
             # exit
             sys.exit(0)
         elif cmd == 1:
+            self.print_menu()
+        elif cmd == 2:
             # set size
             new_size = self.get_int_input()
             self.set_size(new_size)
-        elif cmd == 2:
-            # toggle sorted
-            self.toggle_sorted()
         elif cmd == 3:
-            # list elements
-            size = len(self.a)
-            if size == 0:
-                print('List is empty')
-            else:
-                max_items = 50
-                num_shown_items = 20 if size > max_items else size
-                for i in range(num_shown_items):
-                    print(f'[#{i + 1}]: {self.a[i]}')
-                if size > num_shown_items:
-                    print(f'... {size - num_shown_items - 1} items excluded')
-                    print(f'[#{size}]: {self.a[size - 1]}')
+            self.toggle_sorted()
         elif cmd == 4:
-            # linear search
+            self.list_elements()
+        elif cmd == 5:
+            print('Linear search for value')
             value = self.get_search_value()
             self.linear_search(value)
-        elif cmd == 5:
-            # binary search (disabled if not sorted)
+        elif cmd == 6:
             if self.is_sorted:
+                print('Binary search for value')
                 value = self.get_search_value()
                 self.binary_search(value)
             else:
-                print('Must be sorted')
-        elif cmd == 6:
-            # linear performance test
-            self.run_performance_tests(LinearSearchTimer())
+                print('Must be sorted for binary search')
         elif cmd == 7:
-            # binary performance test (disabled if not sorted)
+            print('Linear search performance test')
+            self.run_performance_tests(LinearSearchTimer())
+        elif cmd == 8:
             if self.is_sorted:
+                print('Binary search performance test')
                 self.run_performance_tests(BinarySearchTimer())
             else:
-                print('Must be sorted')
+                print('Must be sorted for binary search performance tests')
+
+    def list_elements(self):
+        size = len(self.a)
+        if size == 0:
+            print('List is empty')
+        else:
+            max_items = 50
+            num_shown_items = 10 if size > max_items else size
+            for i in range(num_shown_items):
+                print(f'[{i}]: {self.a[i]}')
+            if size > num_shown_items:
+                print(f'... {size - num_shown_items * 2} items excluded')
+            for i in range(size - num_shown_items, size):
+                print(f'[{i}]: {self.a[i]}')
 
     def get_search_value(self):
         if len(self.a) > 0:
@@ -105,23 +112,23 @@ class SearchInterface:
         else:
             random.shuffle(self.a)
 
-    def linear_search(self, value: int) -> (int, float):
+    def linear_search(self, value: int) -> Tuple[int, float]:
         return self.do_search(value, "Linear search", search.linear_search)
 
-    def binary_search(self, value: int) -> (int, float):
+    def binary_search(self, value: int) -> Tuple[int, float]:
         self._validate_sorted()
         return self.do_search(value, "Binary search", search.binary_search)
 
-    def do_search(self, value: int, description: str, search_op: Callable[[Iterable[int], int], int]) -> (int, float):
+    def do_search(self, value: int, description: str, search_op: Callable[[Iterable[int], int], int]) -> Tuple[int, float]:
         start_time = perf_counter()
         try:
             index = search_op(self.a, value)
         except ValueError:
             index = -1
-        elapsed_time = perf_counter() - start_time
+        elapsed_time_ms = (perf_counter() - start_time) * 1000
         print(
-            f'{description} took {elapsed_time * 1000:.4f} ms over {len(self.a):,} items: {index if index > 0 else "not found"}')
-        return index, elapsed_time
+            f'{description} took {elapsed_time_ms:.4f} ms over {len(self.a):,} items: {index if index > 0 else "not found"}')
+        return index, elapsed_time_ms
 
     def _validate_sorted(self):
         if not self.is_sorted:
@@ -134,7 +141,7 @@ class SearchInterface:
         print(f'{search_timer.description} ran in {results.complexity} time')
         for i in range(results.num_sizes):
             print(
-                f'  Average time for {results.num_elements[i]:,} elements: {results.elapsed_times[i] * 1000:.4f} ms')
+                f'  Average time for {results.num_elements[i]:,} elements: {results.elapsed_times[i]:.4f} ms')
 
         return results
 
@@ -146,4 +153,4 @@ class SearchInterface:
 if __name__ == '__main__':
     s = SearchInterface()
     while True:
-        s.print_menu()
+        s.process_input()
