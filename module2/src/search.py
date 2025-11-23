208f46fe652d68b3d1c0076c7f037d5a5b23e9af
Richard Bogart <trbogart@gmail.com>
2025-11-22 17:25:56 -0700

    rename variables

--- module2/src/search.py
+++ module2/src/search.py
@@ -1,22 +1,22 @@
-def linear_search(a, element):
+def linear_search(a, value):
     """
     Performs a linear search for an element in a list.
     :param a: list (or other iterable) to search
-    :param element: the element to search for
+    :param value: the value to search for
     :return: the index of the given element in the list
     :raises ValueError: If element not found
     """
-    for i, value in enumerate(a):
-        if value == element:
+    for i, element in enumerate(a):
+        if element == value:
             return i
     raise ValueError("Element not found")
 
 
-def binary_search(a, element):
+def binary_search(a, value):
     """
     Performs a binary search for an element in a sorted list.
     :param a:  sorted list that supports  random-access retrieval
-    :param element: the element to search for
+    :param value: the value to search for
     :return: the index of the given element in the list
     :raises ValueError: If element not found
     """
@@ -24,9 +24,9 @@ def binary_search(a, element):
     high = len(a) - 1
     while low <= high:
         mid = (low + high) // 2
-        if element == a[mid]:
+        if value == a[mid]:
             return mid
-        elif element < a[mid]:
+        elif value < a[mid]:
             high = mid - 1
         else:
             low = mid + 1
