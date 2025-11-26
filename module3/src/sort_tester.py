771df1ff41b870eec18d39b05ad7219909d07069
Richard Bogart <trbogart@gmail.com>
2025-11-26 16:27:51 -0700

    tester

--- module3/src/sort_tester.py
+++ module3/src/sort_tester.py
@@ -3,7 +3,9 @@ from time import perf_counter
 from data_generator import generate_shuffled, generate_partially_sorted, generate_sorted, generate_reverse_sorted
 from sort import bubble_sort, selection_sort, insertion_sort, merge_sort
 
-sizes = [1000, 5000, 10000, 50000]
+# sizes = [10, 100, 1_000]
+sizes = [1_000, 5_000, 10_000, 50_000]
+
 data_generators = {
     'shuffled': generate_shuffled,
     'already sorted': generate_sorted,
@@ -11,10 +13,10 @@ data_generators = {
     'partially sorted': generate_partially_sorted,
 }
 sort_algorithms = {
-    'Bubble sort': bubble_sort,
-    'Selection sort': selection_sort,
-    'Insertion sort': insertion_sort,
-    'Merge sort': merge_sort,
+    'bubble': bubble_sort,
+    'selection': selection_sort,
+    'insertion': insertion_sort,
+    'merge': merge_sort,
 }
 num_tests = 5
 
@@ -29,12 +31,13 @@ if __name__ == '__main__':
     best_results = []
     for size in sizes:
         for data_type, data_generator in data_generators.items():
+            data_type_csv = data_type.lower().replace(' ', '_')
             print('----------------------------------------------------------------------')
             print(f'Sorting {size:,} {data_type} elements')
             best_sort_type = None
             best_sort_time_ms = float('inf')
             for sort_type, sort_algorithm in sort_algorithms.items():
-                print(f'{sort_type} with {size:,} {data_type} elements...', end='\t')
+                print(f'- {sort_type} sort with {size:,} {data_type} elements...', end='\t')
                 total_time = 0
                 for test in range(num_tests):
                     data = data_generator(size)
@@ -44,22 +47,23 @@ if __name__ == '__main__':
                     validate_sorted(data)
                 average_time_ms = total_time * 1000 / num_tests
                 print(f'{average_time_ms:.1f} ms')
-                results.append((data_type, size, sort_type, average_time_ms))
+                results.append((size, data_type_csv, sort_type, average_time_ms))
 
                 if average_time_ms < best_sort_time_ms:
                     best_sort_time_ms = average_time_ms
                     best_sort_type = sort_type
 
             print(f'Best sort type for {size:,} {data_type} elements: {best_sort_type}')
-            best_results.append((data_type, size, best_sort_type))
+            best_results.append((size, data_type_csv, best_sort_type))
 
     print('----------------------------------------------------------------------')
-    print('all results')
-    print('data_type,size,sort_type,average_time_ms')
-    for data_type, size, sort_type, average_time_ms in results:
-        print(f'{data_type},{size},{sort_type},{average_time_ms:.1f}')
+    print('All results (csv)')
+    print('size,data_type,sort_type,average_time_ms')
+    for size, data_type, sort_type, average_time_ms in results:
+        print(f'{size},{data_type},{sort_type},{average_time_ms:.1f}')
 
     print('----------------------------------------------------------------------')
-    print('data_type,size,best_sort_type')
-    for (data_type, size, best_sort_type) in best_results:
-        print(f'{data_type},{size},{best_sort_type}')
+    print('Best sort type (csv)')
+    print('size,data_type,best_sort_type')
+    for (size, data_type, best_sort_type) in best_results:
+        print(f'{size},{data_type},{best_sort_type}')
