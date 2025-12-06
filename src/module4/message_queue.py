5cec5215244576bee085114e888992a6edcd7358
Richard Bogart <trbogart@gmail.com>
2025-12-05 19:44:31 -0700

    Fix typing hint

--- src/module4/message_queue.py
+++ src/module4/message_queue.py
@@ -45,8 +45,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.linked_list:
         print('Using linked list')
-        queue = LinkedList[float]()
+        queue = LinkedList[str]()
     else:
         print('Using Python list')
-        queue = Queue[float]()
+        queue = Queue[str]()
     MessageQueue(queue).execute()
