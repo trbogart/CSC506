5966034289f130ea37f3fb28298536dff22f7540
Richard Bogart <trbogart@gmail.com>
2025-12-20 17:17:00 -0700

    Module 6 checkpoint

---
+++ src/module6/bst.py
@@ -0,0 +1,286 @@
+import math
+from enum import Enum
+
+from module6.TreePrint import pretty_tree
+
+
+class TraversalOrder(Enum):
+    IN_ORDER = 0
+    PRE_ORDER = 1
+    POST_ORDER = 2
+
+
+class BinarySearchTree:
+    """
+    Binary search tree.
+    Elements must support the > and < operations.
+    """
+
+    class Node:
+        def __init__(self, element):
+            self.element = element
+            self.parent = None
+            self.left = None
+            self.right = None
+            self.height = 0
+
+        def __str__(self):
+            return str(self.element)
+
+        def __repr__(self):
+            return repr(self.element)
+
+        def set_left(self, new_node):
+            """
+            Sets the left node, resetting height as needed.
+            """
+            if new_node is not None:
+                new_node.parent = self
+            self.left = new_node
+            self.reset_height()
+
+        def set_right(self, new_node):
+            """
+            Sets the rights node, resetting height as needed.
+            """
+            if new_node is not None:
+                new_node.parent = self
+            self.right = new_node
+            self.reset_height()
+
+        def reset_height(self):
+            """
+            Recalculates height of node, adjusting ancestors as needed.
+            """
+            new_height = 1 + max(BinarySearchTree._get_height(self.left), BinarySearchTree._get_height(self.right))
+            if self.height != new_height:
+                self.height = new_height
+                if self.parent is not None:
+                    self.parent.reset_height()
+
+        def get_min_node(self):
+            """
+            Get minimum (left-most) node. Will never be None.
+            """
+            node = self
+            while node.left is not None:
+                node = node.left
+            return node
+
+        def get_max_node(self):
+            """
+            Get maximum (right-most) node. Will never be None.
+            """
+            node = self
+            while node.right is not None:
+                node = node.right
+            return node
+
+        def get_previous_node(self):
+            """
+            Gets the previous node, or None if this is the first (min) node.
+            """
+            if self.left is None:
+                if self.parent is not None and self.parent.right == self:
+                    return self.parent
+                return None
+            return self.left.get_max_node()
+
+        def get_next_node(self):
+            """
+            Returns the next node, or None if this is the last (max) node.
+            """
+            if self.right is None:
+                if self.parent is not None and self.parent.left == self:
+                    return self.parent
+                return None
+            return self.right.get_min_node()
+
+        def insert_node(self, new_node):
+            """
+            Inserts a node below the current node.
+            """
+            if new_node.element < self.element:
+                if self.left is None:
+                    self.set_left(new_node)
+                else:
+                    self.left.insert_node(new_node)
+            else:
+                if self.right is None:
+                    self.set_right(new_node)
+                else:
+                    self.right.insert_node(new_node)
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __contains__(self, key):
+        return self.search(key) is not None
+
+    def __iter__(self):
+        return self.traverse()
+
+    def __str__(self):
+        return pretty_tree(self)
+
+    @staticmethod
+    def _get_height(node):
+        """
+        Gets the height of a node, of 0 if None
+        """
+        return node.height if node is not None else -1
+
+    def insert(self, element):
+        """
+        Inserts an element into the tree.
+        :param element: element to insert
+        """
+        if element is None:
+            raise ValueError("element cannot be None")
+        new_node = self.Node(element)
+        if self.root is None:
+            self.root = new_node
+        else:
+            self.root.insert_node(new_node)
+        self.size += 1
+
+    def delete(self, element):
+        """
+        Deletes a matching element from the tree. Returns the matching element if it exists, or None otherwise.
+        """
+
+        def replace_node(node, new_node):
+            # replace node with one of its children
+            if node.parent is None:
+                self.root = new_node
+                if new_node is not None:
+                    new_node.parent = None
+            elif node.parent.left == node:
+                node.parent.set_left(new_node)
+            elif node.parent.right == node:
+                node.parent.set_right(new_node)
+            else:
+                raise ValueError("Invalid node")
+
+        def delete_node(node):
+            # delete this node
+            if node.left is None:
+                # leaf node or right child only
+                replace_node(node, node.right)
+            elif node.right is None:
+                # left child only
+                replace_node(node, node.left)
+            else:
+                # delete node with 2 children
+                if node.right.height > node.left.height:
+                    # replace with next node
+                    replacement_node = node.get_next_node()
+                else:
+                    # replace with previous node
+                    replacement_node = node.get_previous_node()
+                node.element, replacement_node.element = replacement_node.element, node.element
+                delete_node(replacement_node)
+
+        def delete_element(node):
+            if node is None:
+                return None
+            if element < node.element:
+                return delete_element(node.left)
+            elif element > node.element:
+                return delete_element(node.right)
+            else:
+                deleted_element = node.element
+                delete_node(node)
+                return deleted_element
+
+        if element is None:
+            raise ValueError("element cannot be None")
+        deleted = delete_element(self.root)
+        if deleted is not None:
+            self.size -= 1
+        return deleted
+
+    def search(self, element):
+        """
+        Return matching element if present, or None if absent.
+        """
+        node = self.root
+        while node is not None:
+            if element > node.element:
+                node = node.right
+            elif element < node.element:
+                node = node.left
+            else:
+                return node.element  # match
+        return None
+
+    def traverse(self, order: TraversalOrder = TraversalOrder.IN_ORDER):
+        """
+        Returns an in-order iterator.
+        """
+
+        def traverse_in_order(node):
+            if node is not None:
+                if order == TraversalOrder.IN_ORDER:
+                    yield from traverse_in_order(node.left)
+                    yield node.element
+                    yield from traverse_in_order(node.right)
+                elif order == TraversalOrder.PRE_ORDER:
+                    yield node.element
+                    yield from traverse_in_order(node.left)
+                    yield from traverse_in_order(node.right)
+                elif order == TraversalOrder.POST_ORDER:
+                    yield from traverse_in_order(node.left)
+                    yield from traverse_in_order(node.right)
+                    yield node.element
+                else:
+                    raise ValueError("Invalid TraversalOrder")
+
+        return traverse_in_order(self.root)
+
+    def get_min(self):
+        """
+        Returns the minimum element in the tree, or None if empty.
+        """
+        if self.root is None:
+            return None
+        return self.root.get_min_node().element
+
+    def get_max(self):
+        """
+        Returns the maximum value in the tree, or None if empty.
+        """
+        if self.root is None:
+            return None
+        return self.root.get_max_node().element
+
+    def get_height(self):
+        """
+        Returns the height of the tree, or -1 if empty
+        """
+        return self._get_height(self.root)
+
+    def get_balanced_height(self):
+        """
+        Returns the minimum possible height of the tree if it was perfectly balanced. Will return -1 for empty tree.
+        """
+        return int(math.ceil(math.log2(self.size + 1) - 1))
+
+    def get_balance_factor(self):
+        """
+        Returns the balance factor of this tree (ratio of actual height to minimum height).
+        """
+        balanced_height = self.get_balanced_height()
+        if balanced_height > 0:
+            return self.get_height() / balanced_height
+        return 1.0
+
+    def is_balanced(self):
+        """
+        Returns true if this tree is balanced (height is no more than 50% greater than balanced height).
+        """
+        return self.get_balance_factor() < 1.5
---
+++ test/module6/test_bst.py
@@ -0,0 +1,454 @@
+import random
+
+from module6.bst import BinarySearchTree, TraversalOrder
+
+
+def test_empty():
+    bst = BinarySearchTree()
+    assert len(bst) == 0
+    assert bst.get_height() == -1
+    assert bst.search(1) is None
+    assert list(bst.traverse()) == []
+
+
+def test_insert_one_element():
+    bst = BinarySearchTree()
+    bst.insert(1)
+    assert len(bst) == 1
+    assert bst.get_height() == 0
+    assert list(bst.traverse()) == [1]
+    assert bst.search(1) == 1
+    assert bst.search(2) is None
+    assert bst.search(0) is None
+
+
+def test_insert_bigger_element():
+    bst = BinarySearchTree()
+    bst.insert(1)
+    bst.insert(2)
+    assert len(bst) == 2
+    assert bst.get_height() == 1
+    assert list(bst.traverse()) == [1, 2]
+    assert bst.search(1) == 1
+    assert bst.search(2) == 2
+    assert bst.search(0) is None
+    assert bst.search(3) is None
+
+
+def test_insert_smaller_element():
+    bst = BinarySearchTree()
+    bst.insert(2)
+    bst.insert(1)
+    assert len(bst) == 2
+    assert bst.get_height() == 1
+    assert list(bst.traverse()) == [1, 2]
+    assert bst.search(1) == 1
+    assert bst.search(2) == 2
+    assert bst.search(0) is None
+    assert bst.search(3) is None
+
+
+def test_insert_multi():
+    bst = BinarySearchTree()
+    bst.insert(2)
+    bst.insert(1)
+    bst.insert(3)
+    bst.insert(5)
+    bst.insert(4)
+    assert len(bst) == 5
+    assert bst.get_height() == 3
+    assert list(bst.traverse()) == [1, 2, 3, 4, 5]
+    for i in range(1, 6):
+        assert bst.search(i) == i
+
+
+def test_insert_duplicate():
+    bst = BinarySearchTree()
+    bst.insert(2)
+    bst.insert(1)
+    bst.insert(3)
+    bst.insert(3)
+    bst.insert(1)
+    assert len(bst) == 5
+    assert bst.get_height() == 2
+    assert list(bst.traverse()) == [1, 1, 2, 3, 3]
+    assert bst.search(1) == 1
+    assert bst.search(2) == 2
+    assert bst.search(3) == 3
+
+
+def test_insert_rand():
+    random.seed(42)
+    bst = BinarySearchTree()
+    num_elements = 100
+    expected = []
+    for i in range(num_elements):
+        element = random.randint(1, 1000)
+        bst.insert(element)
+        expected.append(element)
+        assert len(bst) == i + 1
+    expected.sort()
+    assert list(bst) == expected
+
+
+def test_insert_linear_right():
+    bst = BinarySearchTree()
+    num_elements = 100
+    for i in range(num_elements):
+        bst.insert(i)
+        assert len(bst) == i + 1
+    assert len(bst) == num_elements
+    assert bst.get_height() == num_elements - 1
+    assert list(bst.traverse()) == [i for i in range(num_elements)]
+
+
+def test_insert_linear_left():
+    bst = BinarySearchTree()
+    num_elements = 100
+    for i in range(num_elements - 1, -1, -1):
+        bst.insert(i)
+        assert len(bst) == 100 - i
+    assert len(bst) == num_elements
+    assert bst.get_height() == num_elements - 1
+    assert list(bst.traverse()) == [i for i in range(num_elements)]
+
+
+def test_balance():
+    bst = BinarySearchTree()
+    bst.insert(10)
+    assert bst.get_height() == 0
+    assert bst.get_balanced_height() == 0
+    assert bst.get_balance_factor() == 1.0
+    bst.insert(5)
+    assert bst.get_height() == 1
+    assert bst.get_balanced_height() == 1
+    assert bst.get_balance_factor() == 1.0
+    bst.insert(3)
+    assert bst.get_height() == 2
+    assert bst.get_balanced_height() == 1
+    assert bst.get_balance_factor() == 2.0
+    bst.insert(15)
+    assert bst.get_height() == 2
+    assert bst.get_balanced_height() == 2
+    assert bst.get_balance_factor() == 1.0
+    bst.insert(12)
+    assert bst.get_height() == 2
+    assert bst.get_balanced_height() == 2
+    assert bst.get_balance_factor() == 1.0
+    bst.insert(1)
+    assert bst.get_height() == 3
+    assert bst.get_balanced_height() == 2
+    assert bst.get_balance_factor() == 3 / 2
+    bst.insert(2)
+    assert bst.get_height() == 4
+    assert bst.get_balanced_height() == 2
+    assert bst.get_balance_factor() == 2.0
+    bst.insert(8)
+    assert bst.get_height() == 4
+    assert bst.get_balanced_height() == 3
+    assert bst.get_balance_factor() == 4 / 3
+
+
+def test_traverse():
+    bst = _get_test_tree()
+    #     ______6__
+    #    /         \
+    #   2____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+    #        /         /
+    #       4         9
+    #      /
+    #     3
+    assert list(bst.traverse()) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert list(bst.traverse(TraversalOrder.IN_ORDER)) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert list(bst.traverse(TraversalOrder.PRE_ORDER)) == [6, 2, 1, 5, 4, 3, 8, 7, 10, 9]
+    assert list(bst.traverse(TraversalOrder.POST_ORDER)) == [1, 3, 4, 5, 2, 7, 9, 10, 8, 6]
+
+
+def test_min_max():
+    bst = _get_test_tree()
+    assert bst.get_min() == 1
+    assert bst.get_max() == 10
+
+
+def test_delete():
+    bst = _get_test_tree()
+    #     ______6__
+    #    /         \
+    #   2____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+    #        /         /
+    #       4         9
+    #      /
+    #     3
+    _test_tree(bst,
+               size=10,
+               min=1,
+               max=10,
+               expected=Node(6,
+                             height=4,
+                             left=Node(2,
+                                       height=3,
+                                       left=Node(1),
+                                       right=Node(5,
+                                                  height=2,
+                                                  left=Node(4,
+                                                            height=1,
+                                                            left=Node(3))),
+                                       ),
+                             right=Node(8,
+                                        height=2,
+                                        left=Node(7),
+                                        right=Node(10,
+                                                   height=1,
+                                                   left=Node(9)))
+                             ))
+    # delete leaf
+    assert bst.delete(9) == 9
+    #     ______6__
+    #    /         \
+    #   2____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+    #        /
+    #       4
+    #      /
+    #     3
+    _test_tree(bst,
+               size=9,
+               min=1,
+               max=10,
+               expected=Node(6,
+                             height=4,
+                             left=Node(2,
+                                       height=3,
+                                       left=Node(1),
+                                       right=Node(5,
+                                                  height=2,
+                                                  left=Node(4,
+                                                            height=1,
+                                                            left=Node(3))),
+                                       ),
+                             right=Node(8,
+                                        height=1,
+                                        left=Node(7),
+                                        right=Node(10))
+                             ))
+
+    # delete node with left child only
+    assert bst.delete(4) == 4
+    #     ______6__
+    #    /         \
+    #   2____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+    #        /
+    #       3
+    _test_tree(bst,
+               size=8,
+               min=1,
+               max=10,
+               expected=Node(6,
+                             height=3,
+                             left=Node(2,
+                                       height=2,
+                                       left=Node(1),
+                                       right=Node(5,
+                                                  height=1,
+                                                  left=Node(3)),
+                                       ),
+                             right=Node(8,
+                                        height=1,
+                                        left=Node(7),
+                                        right=Node(10))
+                             ))
+
+    # delete node with 2 children
+    assert bst.delete(2) == 2
+    #     ______6__
+    #    /         \
+    #   3____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+
+    _test_tree(bst,
+               size=7,
+               min=1,
+               max=10,
+               expected=Node(6,
+                             height=2,
+                             left=Node(3,
+                                       height=1,
+                                       left=Node(1),
+                                       right=Node(5),
+                                       ),
+                             right=Node(8,
+                                        height=1,
+                                        left=Node(7),
+                                        right=Node(10))
+                             ))
+
+    # delete root with 2 children
+    assert bst.delete(6) == 6
+    #     5__
+    #    /   \
+    #   3     8
+    #  /     / \
+    # 1     7   10
+    _test_tree(bst,
+               size=6,
+               min=1,
+               max=10,
+               expected=Node(5,
+                             height=2,
+                             left=Node(3,
+                                       height=1,
+                                       left=Node(1)
+                                       ),
+                             right=Node(8,
+                                        height=1,
+                                        left=Node(7),
+                                        right=Node(10))
+                             ))
+
+    assert bst.delete(7) == 7
+    #     5__
+    #    /   \
+    #   3     8
+    #  /       \
+    # 1         10
+    _test_tree(bst,
+               size=5,
+               min=1,
+               max=10,
+               expected=Node(5,
+                             height=2,
+                             left=Node(3,
+                                       height=1,
+                                       left=Node(1)
+                                       ),
+                             right=Node(8,
+                                        height=1,
+                                        right=Node(10))
+                             ))
+
+    # delete node with right child
+    assert bst.delete(10) == 10
+    #     5__
+    #    /   \
+    #   3     8
+    #  /
+    # 1
+    _test_tree(bst,
+               size=4,
+               min=1,
+               max=8,
+               expected=Node(5,
+                             height=2,
+                             left=Node(3,
+                                       height=1,
+                                       left=Node(1)
+                                       ),
+                             right=Node(8)
+                             ))
+
+    assert bst.delete(8) == 8
+    #     5
+    #    /
+    #   3
+    #  /
+    # 1
+    _test_tree(bst,
+               size=3,
+               min=1,
+               max=5,
+               expected=Node(5,
+                             height=2,
+                             left=Node(3,
+                                       height=1,
+                                       left=Node(1)
+                                       )))
+
+    # delete root with left child
+    assert bst.delete(5) == 5
+    #  3
+    # /
+    # 1
+    _test_tree(bst,
+               size=2,
+               min=1,
+               max=3,
+               expected=Node(3,
+                             height=1,
+                             left=Node(1)
+                             ))
+    # delete root with left child
+    assert bst.delete(3) == 3
+    # 1
+    assert type(bst.root) == BinarySearchTree.Node
+    assert len(bst) == 1
+    assert bst.root.element == 1
+    assert bst.get_height() == 0
+    assert bst.root.height == 0
+
+    _test_tree(bst, min=1, max=1, size=1, expected=Node(1))
+
+    # delete last node
+    assert bst.delete(1) == 1
+    assert len(bst) == 0
+    assert bst.get_height() == -1
+    assert list(bst) == []
+    assert bst.root is None
+
+
+class Node:
+    def __init__(self, element, height=0, left=None, right=None):
+        self.element = element
+        self.height = height
+        self.left = left
+        self.right = right
+
+
+def _test_tree(bst, size, min, max, expected):
+    assert len(bst) == size
+    assert bst.get_min() == min
+    assert bst.get_max() == max
+    assert bst.get_height() == expected.height
+    _test_node(bst.root, expected)
+
+
+def _test_node(node, expected):
+    if expected is None:
+        assert node is None
+    else:
+        assert node is not None
+        assert node.element == expected.element
+        assert node.height == expected.height
+        _test_node(node.left, expected.left)
+        _test_node(node.right, expected.right)
+
+
+def _get_test_tree():
+    #     ______6__
+    #    /         \
+    #   2____       8__
+    #  /     \     /   \
+    # 1       5   7     10
+    #        /         /
+    #       4         9
+    #      /
+    #     3
+    bst = BinarySearchTree()
+    bst.insert(6)
+    bst.insert(2)
+    bst.insert(5)
+    bst.insert(1)
+    bst.insert(8)
+    bst.insert(4)
+    bst.insert(7)
+    bst.insert(10)
+    bst.insert(3)
+    bst.insert(9)
+    return bst
